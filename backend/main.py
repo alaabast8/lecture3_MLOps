997640cbf73b9ad928d9ebea0b19df8962d10604
Alaa Albast <eduway.dev@yahoo.com>
2026-01-08 20:23:18 +0200

    fixing the app backend testing

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 import os
 from dotenv import load_dotenv
+from sqlalchemy.orm import declarative_base
 
 # 1. Load Environment Variables
 load_dotenv()
